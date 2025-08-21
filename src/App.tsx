6fdc5e936f72437346a605ac944d37b372c0077d
sohelranaweb <sohel.rana.web2@gmail.com>
2025-08-21 09:27:19 +0600

    project setup done

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-xl font-bold">Welcome to DigiCash App</h1>
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
